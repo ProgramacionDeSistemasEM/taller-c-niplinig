ee06488a4c7992805ab6e3eb04d756e2e1749b19
niplinig <niplinig@espol.edu.ec>
2021-10-19 14:26:21 -0500

    Primer Commit

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
- 
+
 int main(int argc, char **argv){
-	
+
 	int elementos = 0;
 	char c = 0;
-	
-	
+
+
+    
 	//Aqui manejamos las opciones.
-	//El argumento -p tiene un argumento, que es 
+	//El argumento -p tiene un argumento, que es
 	//el numero de elementos a pedir (por eso el :)
 	while ((c = getopt (argc, argv, "p:")) != -1){
 		switch(c){
@@ -26,12 +27,12 @@ int main(int argc, char **argv){
 	//Puede declarar más variables si lo necesita.
 	float sum = 0.0f;
 	float max_imc = 0.0f;
-	
-	
-	
-	
+
+
+
+
 	//No modifique estas lineas
-	//Guarde los resultados en las 
+	//Guarde los resultados en las
 	//variables sum y max_imc
 	printf("\npromedio IMC: %.1f\n", sum);
 	printf("maximo IMC: %.1f\n", max_imc);
